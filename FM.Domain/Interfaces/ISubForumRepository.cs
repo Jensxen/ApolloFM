7817ffddceb95181c5d3f3a5a8dc231784526889
M. <146251152+Jensxen@users.noreply.github.com>
2025-03-21 09:13:09 +0100

    SubForumRepository

---
+++ FM.Domain/Interfaces/ISubForumRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FM.Domain.Entities;
+
+namespace FM.Domain.Interfaces
+{
+    public interface ISubForumRepository
+    {
+        Task<IEnumerable<SubForum>> GetAllSubForums();
+        Task<SubForum> GetSubForumById(int id);
+        Task AddSubForum(SubForum subForum);
+        Task UpdateSubForum(SubForum subForum);
+        Task DeleteSubForum(int id);
+    }
+}
---
+++ FM.Infrastructure/Repositories/SubForumRepository.cs
@@ -0,0 +1,57 @@
+﻿using FM.Domain.Entities;
+using FM.Domain.Interfaces;
+using FM.Infrastucture.DbContext;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FM.Infrastructure.Repositories
+{
+    public class SubForumRepository : ISubForumRepository
+    {
+        private readonly ApolloContext _context;
+
+        public SubForumRepository(ApolloContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddSubForum(SubForum subForum)
+        {
+            _context.SubForums.Add(subForum);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteSubForum(int id)
+        {
+            var post = await _context.SubForums.FindAsync(id);
+            if (post != null)
+            {
+                _context.SubForums.Remove(post);
+            }
+        }
+
+        public async Task<IEnumerable<SubForum>> GetAllSubForums()
+        {
+            return await _context.SubForums
+                .Include(s => s.Posts)
+                .ToListAsync();
+        }
+
+        public async Task<SubForum> GetSubForumById(int id)
+        {
+            return await _context.SubForums
+                .Include(s => s.Posts)
+                .FirstOrDefaultAsync(s => s.Id == id);
+        }
+
+        public async Task UpdateSubForum(SubForum subForum)
+        {
+            _context.SubForums.Update(subForum);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
